64160eae4a0bdbb609f7ef3161bb04792beb1ba8
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-23 18:42:08 +0530

    [PALLAVI] Add: File

---
+++ RegularExpressionPracticeProblem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegularExpressionPracticeProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        { 
+                string input = "abbb";
+
+
+                string pattern = "^a{1}b{2,3}$";
+
+
+                Regex regex = new Regex(pattern);
+
+
+                if (regex.IsMatch(input))
+                {
+                    Console.WriteLine("String matched the pattern!");
+                }
+                else
+                {
+                    Console.WriteLine("String did not match the pattern!");
+                }
+            
+        }
+    }
+    
+}
